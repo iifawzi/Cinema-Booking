2edf2a75035f6c3df4ef37cdd93c317dd415b583
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-06-29 12:35:34 +0200

    First init

---
+++ server.js
@@ -0,0 +1,8 @@
+const app = require("./app");
+const http = require("http");
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+server.listen(3000, ()=>{
+    console.log(`Server is listening on Port ${port}`);
+})
