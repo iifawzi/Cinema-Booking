549691e97c47945be3257867aec1994cad03b202
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-07-07 19:45:52 +0200

    phone number is already registered

--- services/users/users.controller.js
+++ services/users/users.controller.js
@@ -2,12 +2,14 @@ const {ErrorHandler} = require("../../helpers/error");
 const respond = require("../../helpers/respond");
 const userService = require("./users.service");
 const {createToken} = require("../../helpers/jwt");
+
+// For adding new User: 
 const signup = async (req,res,next)=>{
     try {
         const userData = req.body;
         const userExist = await userService.checkIfPhoneExists(userData.phone_number);
         if (userExist){
-            throw new ErrorHandler(409, "Phone number already registered");
+            throw new ErrorHandler(409, "Phone number is already registered");
         }
         const createdUser = await userService.createNewUser(userData);
         if (createdUser){
