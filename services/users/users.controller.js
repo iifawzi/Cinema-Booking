57d0c157e0d96ada395e00c9c0d9bf4a902ce5d5
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-07-07 18:00:38 +0200

    change the way of calling

--- services/users/users.controller.js
+++ services/users/users.controller.js
@@ -1,11 +1,13 @@
 const {handleError,ErrorHandler} = require("../../helpers/error");
-const {checkIfPhoneExists} = require("./users.service");
+const userService = require("./users.service");
 const signup = async (req,res,next)=>{
     try {
-        const userExist = await checkIfPhoneExists(req.body.phone_number);
+        const {phone_number} = req.body;
+        const userExist = await userService.checkIfPhoneExists(phone_number);
         if (userExist){
             throw new ErrorHandler(409, "Phone number already registered");
         }
+
     }catch(err) {
         next(err);
     }
