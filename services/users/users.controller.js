ccb08497b6cfe35ab3075fcd40ef4a6c0f729e87
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-07-07 18:17:11 +0200

    register user successfully

--- services/users/users.controller.js
+++ services/users/users.controller.js
@@ -1,13 +1,17 @@
 const {ErrorHandler} = require("../../helpers/error");
+const respond = require("../../helpers/respond");
 const userService = require("./users.service");
 const signup = async (req,res,next)=>{
     try {
-        const {phone_number} = req.body;
-        const userExist = await userService.checkIfPhoneExists(phone_number);
+        const userData = req.body;
+        const userExist = await userService.checkIfPhoneExists(userData.phone_number);
         if (userExist){
             throw new ErrorHandler(409, "Phone number already registered");
         }
-
+        const createdUser = await userService.createNewUser(userData);
+        if (createdUser){
+            return respond(true,201,"done",res);
+        }
     }catch(err) {
         next(err);
     }
--- services/users/users.service.js
+++ services/users/users.service.js
@@ -1,6 +1,10 @@
 const usersModel = require("./users.model");
 
 exports.checkIfPhoneExists = async (phone_number)=>{
-    const userWithThisPhone =  await usersModel.findOne({phone_number});
+    const userWithThisPhone =  await usersModel.findOne({where: {phone_number}});
     return userWithThisPhone;
+};
+exports.createNewUser = async (data)=>{
+    const createdUser = await usersModel.create(data);
+    return createdUser.dataValues;
 };
