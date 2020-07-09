bf6aba74b0ddb5fbd9e0e88bd6b64fd20918118c
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-07-09 19:30:47 +0200

    add test

--- server.js
+++ server.js
@@ -5,3 +5,4 @@ const server = http.createServer(app);
 server.listen(3000, ()=>{
     console.log(`Server is listening on Port ${port}`);
 });
+module.exports = server;
---
+++ services/users/users.test.js
@@ -0,0 +1,47 @@
+const request = require("supertest");
+const app = require("../../server");
+var expect = require("chai").expect;
+let server;
+
+describe("/api/auth",async()=>{
+    beforeEach(()=>{
+        server = require("../../app");
+    });
+
+    describe("users", () => {
+        it("Should return 400 if schema validation fails", async () => {
+            const res = await request(app)
+                .post("/api/users/signup")
+                .send({
+                    userId: 1,
+                });
+            expect(res.statusCode).equals(400);
+        });
+        it("Should return 201 if user registered successfully", async () => {
+            const res = await request(app)
+                .post("/api/users/signup")
+                .send({
+                    "phone_number":"01090243795",
+                    "first_name":"fawzi",
+                    "last_name":"essam",
+                    "country":"egypt",
+                    "city":"portsaid",
+                    "firebase_token":"93484893983498"
+                });
+            expect(res.statusCode).equals(201);
+        });
+        it("Should return 409 if Phone is already registered", async () => {
+            const res = await request(app)
+                .post("/api/users/signup")
+                .send({
+                    "phone_number":"01090243795",
+                    "first_name":"fawzi",
+                    "last_name":"essam",
+                    "country":"egypt",
+                    "city":"portsaid",
+                    "firebase_token":"93484893983498"
+                });
+            expect(res.statusCode).equals(409);
+        });
+    });
+});
