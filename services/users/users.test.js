1ee39e98fc2716e3578475ed23da210c0051338e
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-07-10 21:17:57 +0200

    test the refresh_token endpoint

--- services/users/users.test.js
+++ services/users/users.test.js
@@ -5,6 +5,7 @@ let expect = require("chai").expect;
 let server;
 
 describe("/api/users",async()=>{
+    
     describe("/signup", () => {
         it("Should respond 400 if schema validation fails", async () => {
             const res = await request(app)
@@ -78,6 +79,7 @@ describe("/api/users",async()=>{
             await userService.deleteUser("01090243796");
         });
     });
+    
     describe("/refresh_token", ()=>{
         it("Should respond 401 if no token was found", async ()=>{
             const res = await request(app)
@@ -86,6 +88,68 @@ describe("/api/users",async()=>{
                     "refresh_token":"4987478947894879jydkhjdkjhdkjh",
                 });
             expect(res.statusCode).equals(401);
+        });
+        it("Should respond 401 if token is not found", async ()=>{
+            const res = await request(app)
+                .post("/api/users/refresh_token")
+                .send({
+                    "refresh_token":"4987478947894879jydkhjdkjhdkjh",
+                });
+            expect(res.statusCode).equals(401);
+        });
+        it("Should respond 401 if token is invalid", async ()=>{
+            const res = await request(app)
+                .post("/api/users/refresh_token")
+                .set({authorization: "Bearer djkhdhjkdhkjdkjhhdjkjdhkkjh478478874gufy"})    
+                .send({
+                    "refresh_token":"4987478947894879jydkhjdkjhdkjh",
+                });
+            expect(res.statusCode).equals(401);
+        });
+        it("Should respond 401 if refresh_token not match the user_id from token - in db", async ()=>{
+            let res = await request(app)
+                .post("/api/users/signup")
+                .send({
+                    "phone_number":"01090243796",
+                    "first_name":"fawzi",
+                    "last_name":"essam",
+                    "country":"egypt",
+                    "city":"portsaid",
+                    "firebase_token":"93484893983498"
+                });
+            expect(res.statusCode).equals(201);
+            const token = res.body.data.token;
+            res = await request(app)
+                .post("/api/users/refresh_token")
+                .set({authorization: "Bearer"+token})    
+                .send({
+                    "refresh_token":"4987478947894879jydkhjdkjhdkjh",
+                });
+            expect(res.statusCode).equals(401);
+            await userService.deleteUser("01090243796");
+        });
+        it("Should respond 200 if token updated successfully", async ()=>{
+            let res = await request(app)
+                .post("/api/users/signup")
+                .send({
+                    "phone_number":"01090243796",
+                    "first_name":"fawzi",
+                    "last_name":"essam",
+                    "country":"egypt",
+                    "city":"portsaid",
+                    "firebase_token":"93484893983498"
+                });
+            expect(res.statusCode).equals(201);
+            const token = res.body.data.token;
+            const refresh_token = res.body.data.refresh_token;
+            res = await request(app)
+                .post("/api/users/refresh_token")
+                .set({authorization: "Bearer "+token})    
+                .send({
+                    "refresh_token":refresh_token,
+                });
+            expect(res.statusCode).equals(200);
+            await userService.deleteUser("01090243796");
         });
     });
 });
