a39ada2d45fa08c1710405c42bbdf94727fafeb1
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-07-11 12:42:08 +0200

    change the name of allowedFor middleware

--- services/movies/movies.route.js
+++ services/movies/movies.route.js
@@ -3,7 +3,7 @@ const isAuth = require("../../middlewares/is-auth");
 const validate = require("../../helpers/validation");
 const moviesController = require("./movies.controller");
 const moviesSchema = require("./movies.validation");
-const isAllowed = require("../../middlewares/allowed-for");
+const isAllowed = require("../../middlewares/is-allowed");
 const router = express.Router();
 
 
