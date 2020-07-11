a500c5eb102a911aa0f1f38db9fa1d2a18a5ba3a
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-07-11 12:15:22 +0200

    add test for add movie

--- services/movies/movies.test.js
+++ services/movies/movies.test.js
@@ -4,42 +4,34 @@ const { createToken } = require("../../helpers/jwt");
 let expect = require("chai").expect;
 
 
-describe("/moddilewares/is-auth", async()=>{
-    it("Should return 401 if authorization header is not found", async () => {
-        const res = await request(app)
-            .get("/api/welcome");
-        expect(res.statusCode).equals(401);
+describe("/api/movies", async()=>{
+    describe("/add", ()=>{
+        it ("Should respond 400 if schema validation fails", async()=>{
+            const token = createToken({phone_number: "01090243795", admin_id: 1,role:"admin"});
+            const res = await request(app)
+                .post("/api/movies/add")
+                .set({authorization: "Bearer "+token})
+                .send({
+                    "movie_name":"The biggest one2",
+                    "cover": "https://www.google.com2", 
+                    "category": "action", 
+                });
+            expect(res.statusCode).equals(400);
+        });
+        it ("Should respond 201 if created successfully", async()=>{
+            const token = createToken({phone_number: "01090243795", admin_id: 1,role:"admin"});
+            const res = await request(app)
+                .post("/api/movies/add")
+                .set({authorization: "Bearer "+token})
+                .send({
+                    "movie_name":"The biggest one2",
+                    "cover": "https://www.google.com2", 
+                    "category": "action", 
+                    "description": "The best movie ever2", 
+                    "rate": 4
+                });
+            expect(res.statusCode).equals(400);
+        });
     });
-    it("Should return 401 if authorization token is not found", async () => {
-        const res = await request(app)
-            .get("/api/welcome")
-            .set({authorization: "Bearer"});
-        expect(res.statusCode).equals(401);
     });
-    it("Should return 401 if authorization token is not valid", async () => {
-        const res = await request(app)
-            .get("/api/welcome")
-            .set({authorization: "Bearer djkhdhjkdhkjdkjhhdjkjdhkkjh478478874gufy"});
-        expect(res.statusCode).equals(401);
-    });
-    it("Should return 200 if authorization token is correct and not expired", async () => { 
-        const token = createToken({phone_number: "01090243795"});
-        const res = await request(app)
-            .get("/api/welcome")
-            .set({authorization: "Bearer "+token});
-        expect(res.statusCode).equals(200);
-    });
-    /*
-
-    Edit the expire to 0s and test the one below: 
-
-    */
-
-    // it("Should return 401 if authorization token is expired", async () => { 
-    //     const token = createToken({phone_number: "01090243795"});
-    //     const res = await request(app)
-    //         .get("/api/welcome")
-    //         .set({authorization: "Bearer "+token});
-    //     expect(res.statusCode).equals(401);
-    // });
 });
