142f011ef622fe6575de8e105994e15d6d82430f
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-08-08 18:29:55 +0200

    add ondelete cascade

--- services/slots/slots.model.js
+++ services/slots/slots.model.js
@@ -14,6 +14,7 @@ const slotsModel = db.define("slots", {
             model: 'movies',
             key: 'movie_id'
           },
+          onDelete:"CASCADE" // untill this moment, this related to the deleteMovie api which will be edited later.
     },
     hall_id: {
         type: Sequelize.INTEGER,
