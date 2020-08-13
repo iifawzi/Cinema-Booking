585781a4fbc3ea59361c79a957521f36f83771db
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-08-13 10:34:30 +0200

    add toggleCinemaStatus' api

--- services/cinemas/cinemas.test.js
+++ services/cinemas/cinemas.test.js
@@ -152,4 +152,54 @@ describe("/api/cinemas", async()=>{
         });
 
     });
+
+    describe("/toggleCinemaStatus", ()=>{
+        it ("Should respond 400 if schema validation fails", async()=>{
+            const token = createToken({phone_number: "01090243795", admin_id: 1,role:"admin"});
+            const res = await request(app)
+                .patch("/api/cinemas/toggleCinemaStatus")
+                .set({authorization: "Bearer "+token})
+                .send({
+                    "cinema_name": "Crwan",
+                });
+            expect(res.statusCode).equals(400);
+        });
+        it ("Should respond 404 if cinema is not found", async()=>{ 
+            const token = createToken({phone_number: "01090243795", admin_id: 1,role:"admin"});
+            const res = await request(app)
+                .patch("/api/cinemas/toggleCinemaStatus")
+                .set({authorization: "Bearer "+token})
+                .send({
+                    "cinema_id": 2837
+                });
+            expect(res.statusCode).equals(404);
+        });
+        it ("Should respond 200 if update the cinema's status successfully", async()=>{ 
+            const token = createToken({phone_number: "01090243795", admin_id: 1,role:"admin"});
+            let res = await request(app)
+                .post("/api/cinemas/addCinema")
+                .set({authorization: "Bearer "+token})
+                .send({
+                    "cinema_name": "El Crwan",
+                    "cinema_description": "the best in the world",
+                    "cinema_logo": "www.imagelink.com",
+                    "username": "crowann", 
+                    "password": "12qwaszx",
+                    "contact_number": "01090243795",
+                    "country":"Egypt",
+                    "city": "Port Said"
+                });
+            expect(res.statusCode).equals(201);
+            const cinema_id = res.body.data.cinema_id;
+            res = await request(app)
+                .patch("/api/cinemas/toggleCinemaStatus")
+                .set({authorization: "Bearer "+token})
+                .send({
+                    "cinema_id": cinema_id
+                });
+            expect(res.statusCode).equals(200);
+            await deleteCinema(cinema_id);
+        });
+
+    });
 });
